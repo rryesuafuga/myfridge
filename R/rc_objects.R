6d0d71c35127d1e4e9f5bc23ca09aaa8d9d56fd1
rryesuafuga <sseguya256@gmail.com>
2018-10-20 09:17:01 +0200

    namespace issues

--- R/rc_objects.R
+++ R/rc_objects.R
@@ -16,16 +16,16 @@
                   stopifnot(is.data.frame(fridge_content))
                   stopifnot(ncol(fridge_content)==3) 
                   
-                  fridge_content <-
+                  fridge_content <<-
                     setNames(data.frame(matrix(ncol = 3, nrow = 0)), 
                              c("unique_ID", "amount", "unit_space"))
                   
                   
                   # more calculations needed here
-                  volume_taken <- as.numeric(as.matrix(fridge_content[,2]) %*%
+                  volume_taken <<- as.numeric(as.matrix(fridge_content[,2]) %*%
                                       as.matrix(fridge_content[,3]))
                   
-                  free_space <- fridge_volume - volume_taken
+                  free_space <<- fridge_volume - volume_taken
                   
                   .self$fridge_content <- fridge_content
                   .self$fridge_volume <- fridge_volume
