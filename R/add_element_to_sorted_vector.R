bea2c3483aadf48a52285e505276bb10141be1e0
rryesuafuga <sseguya256@gmail.com>
2018-10-21 18:57:01 +0200

    add element to sorted vector working but not well

---
+++ R/add_element_to_sorted_vector.R
@@ -0,0 +1,31 @@
+
+
+
+
+add_element_to_sorted_vector <- function(v, n) 
+  {
+  
+  stopifnot(is.vector(v))
+  stopifnot(is.numeric(n))
+  
+  v_new <-vector()
+  
+  for(i in 1:length(v)) {
+    
+    # less than n
+    if(n < v[i])  v_new <- c(v_new, v[i])
+    
+    # greater than n but n not in v
+    if((n > v[i]) & (!(n %in% v_new)))  v_new <- c(v_new, n)
+    
+    # greater than n but n already in v
+    if((n > v[i]) & (n %in% v_new))   v_new <- c(v_new, v[i])
+    
+    print(v_new)
+  }
+  
+  return(v_new)
+  
+}
+
+  
--- R/rc_objects.R
+++ R/rc_objects.R
@@ -7,7 +7,8 @@
               fields = list(fridge_content = "data.frame", 
                             fridge_volume = "numeric",
                             volume_taken = "numeric",
-                            free_space = "numeric"),
+                            free_space = "numeric",
+                            added_fridge_content = "data.frame"),
               
               methods = list(
                 
@@ -18,7 +19,7 @@
                   
                   fridge_content <<-
                     setNames(data.frame(matrix(ncol = 3, nrow = 0)), 
-                             c("unique_ID", "amount", "unit_space"))
+                             c("product_id", "amount", "unit_space"))
                   
                   
                   # more calculations needed here
@@ -32,13 +33,47 @@
                   .self$fridge_volume <- fridge_volume
                   .self$volume_taken <- volume_taken
                   .self$free_space <- free_space
+                  .self$added_fridge_content <- added_fridge_content
                   .self
     
                 }
                 
-                # more methods follow here
-                
-                
+                # ,
+                # 
+                # 
+                # # more methods follow here
+                # 
+                # add_to_fridge <- function(added_fridge_content){
+                #   
+                #   stopifnot(is.data.frame(added_fridge_content))
+                #   stopifnot(ncol(added_fridge_content)==3)
+                #   
+                #   added_fridge_content <-
+                #     setNames(data.frame(matrix(ncol = 3, nrow = 0)), 
+                #              c("product_id", "amount", "unit_space"))
+                  
+                  # if(added_fridge_content$product_id %in% 
+                  #    fridge_content$product_id){
+                  #   
+                  #   fridge_content$amount[ which(added_fridge_content$product_id %in% 
+                  #   fridge_content$product_id)] <<-
+                  #   added_fridge_content$amount +
+                  #   fridge_content$amount[ which(added_fridge_content$product_id %in% 
+                  #   fridge_content$product_id)]
+                  #   
+                  # }
+                  #   
+                  # else(!(added_fridge_content$product_id %in% 
+                  #           fridge_content$product_id)) 
+                      # fridge_content <<- 
+                      #   rbind(fridge_content, added_fridge_content)
+                    
+                  # }
+                  
+                  
+                  
+                  
+      
               )
               
               
