b8e246eefc16e8840a76faff0a1e0aa0ecf706b2
rryesuafuga <sseguya256@gmail.com>
2018-10-21 23:50:31 +0200

    enough for test_that practice

--- tests/testthat/test_add_element_to_sorted_vector.R
+++ tests/testthat/test_add_element_to_sorted_vector.R
@@ -1,9 +1,22 @@
 
+context("testing add element to sorted vector")
 
-context("add element to sorted vector test")
+v <- c(1:4,6)
+x <- 5
 
-test_that("  ", {
+
+test_that("checking for erroneous input", {
   
+  expect_error(add_element_to_sorted_vector("hej", 5))
+  expect_error(add_element_to_sorted_vector(c(1:4,6), "five"))
   
+})
+
+
+test_that("checking if vector is returned", {
+  
+  ans <- add_element_to_sorted_vector(v,x)
+  expect_true(is.vector(ans))
+  expect_equal(ans, c(1,2,3,4,5,6))
   
 })
