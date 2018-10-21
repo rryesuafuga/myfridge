ba66a35a47036221f32356c409d02ebad8a64894
rryesuafuga <sseguya256@gmail.com>
2018-10-21 19:46:13 +0200

    works

--- R/add_element_to_sorted_vector.R
+++ R/add_element_to_sorted_vector.R
@@ -12,14 +12,19 @@ add_element_to_sorted_vector <- function(v, n)
   
   for(i in 1:length(v)) {
     
+    
     # less than n
-    if(n < v[i])  v_new <- c(v_new, v[i])
-    
+    if(n > v[i])  { v_new <- c(v_new, v[i])
+
     # greater than n but n not in v
-    if((n > v[i]) & (!(n %in% v_new)))  v_new <- c(v_new, n)
-    
+    } else if((n > v[i]) & (!(n %in% v_new)))  { v_new <- c(v_new, n)
+
     # greater than n but n already in v
-    if((n > v[i]) & (n %in% v_new))   v_new <- c(v_new, v[i])
+    # if((n > v[i]) & (n %in% v_new))
+    } else { v_new <- c(v_new, v[i]) }
+    
+    
+
     
     print(v_new)
   }
